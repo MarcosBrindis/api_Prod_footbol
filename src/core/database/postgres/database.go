e57eee9339ea6b7a85fdbf7166e22c060a8d6d79
MarcosBrindis <markos_brind95@hotmail.com>
2025-03-10 23:18:22 -0600

    despliegue

--- src/core/database/postgres/database.go
+++ src/core/database/postgres/database.go
@@ -8,7 +8,8 @@ import (
 )
 
 func NewDatabase() (*sql.DB, error) {
-	connStr := "user=postgres dbname=gameparty sslmode=disable"
+	connStr := "host=52.73.31.113 user=postgres password=123456789a dbname=postgres sslmode=disable"
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
